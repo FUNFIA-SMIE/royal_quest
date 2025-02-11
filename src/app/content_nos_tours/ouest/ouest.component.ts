d5ac51271761ca56629656c6dafb7d468322f1a6
FUNFIA-SMIE <informaticienfunfiasmie@gmail.com>
2025-02-11 16:40:37 +0300

    456

--- src/app/content_nos_tours/ouest/ouest.component.ts
+++ src/app/content_nos_tours/ouest/ouest.component.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-
+import { NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap';
+import { NgbCarouselModule } from '@ng-bootstrap/ng-bootstrap';
 @Component({
   selector: 'app-ouest',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,NgbCarouselModule],
   templateUrl: './ouest.component.html',
   styleUrl: './ouest.component.scss'
 })
@@ -21,4 +22,14 @@ export class OuestComponent {
       '/Pangalanes/Pangalane1.jpg'
     ]
   ];
+
+  showNavigationArrows = false;
+	showNavigationIndicators = false;
+	images = [1055, 194, 368].map((n) => `https://picsum.photos/id/${n}/900/500`);
+
+	constructor(config: NgbCarouselConfig) {
+		// customize default values of carousels used by this component tree
+		config.showNavigationArrows = true;
+		config.showNavigationIndicators = true;
+	}
 }
