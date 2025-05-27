5f3b754c3a61067989650db5272aefeddc8c8f2f
FUNFIA-SMIE <informaticienfunfiasmie@gmail.com>
2025-05-27 12:18:50 +0300

    f

--- dist/frontend/server/angular-app-manifest.mjs
+++ dist/frontend/server/angular-app-manifest.mjs
@@ -8,14 +8,14 @@ export default {
   entryPointToBrowserMapping: {
   "node_modules/@angular/animations/fesm2022/browser.mjs": [
     {
-      "path": "chunk-AXZB4TTI.js",
+      "path": "chunk-BF3GZZ7S.js",
       "dynamicImport": false
     }
   ]
 },
   assets: {
-    'index.csr.html': {size: 108817, hash: 'dddc401704de7c88771075f43cf293bf3f50094375766ae5c04ea0b529ba5a8a', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 52550, hash: '84344243b9e72fda1be203fd0e9defac8e5366b7d1c4e8f12ee5e3b6aa06ea80', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 108817, hash: 'b08c958eeead0587e74c2a35784d417e69acbc8dbf40fe3786dce60eff8df247', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 52550, hash: 'b19e10980dbf9a789abd8edca02e669d9f339b6667435f820e0c9742ff33da3b', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
     'styles-OQDSVMIW.css': {size: 565830, hash: 'Ltm/kswdZtM', text: () => import('./assets-chunks/styles-OQDSVMIW_css.mjs').then(m => m.default)}
   },
 };
