4e47ab2015f8d2b6e01007c3b055a5d969290b23
funfia-smie <informaticienfunfiasmie@gmail.com>
2025-06-04 14:37:29 +0300

    f8

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,31 @@
 import { Component, computed, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from "./navbar/navbar.component";
-import {TranslateModule} from "@ngx-translate/core";
-import {TranslateService} from "@ngx-translate/core";
+import { TranslateModule } from "@ngx-translate/core";
+import { TranslateService } from "@ngx-translate/core";
 import { FooterComponent } from './footer/footer.component';
 
 
 @Component({
-    selector: 'app-root',
-    standalone:true,
-    imports: [
-      RouterOutlet,
-      NavbarComponent,
-      TranslateModule,
-      FooterComponent
-    ],
-    templateUrl: './app.component.html',
-    styleUrl: './app.component.scss'
+  selector: 'app-root',
+  standalone: true,
+  imports: [
+    RouterOutlet,
+    NavbarComponent,
+    TranslateModule,
+    FooterComponent
+  ],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'frontend';
 
-  
+  redirectToWhatsApp(phoneNumber: string) {
+    const url = `https://wa.me/${phoneNumber}`;
+    window.open(url, '_blank');
+  }
+
 
 
   constructor(private translate: TranslateService) {
--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -4,53 +4,53 @@ import { FormsModule } from '@angular/forms';
 import Swal from 'sweetalert2';
 
 @Component({
-    selector: 'app-footer',
-    standalone:true,
-    imports: [FormsModule],
-    templateUrl: './footer.component.html',
-    styleUrl: './footer.component.scss'
+  selector: 'app-footer',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './footer.component.html',
+  styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
-    redirectToWhatsApp(phoneNumber: string) {
-        const url = `https://wa.me/${phoneNumber}`;
-        window.open(url, '_blank');
-      }
-      
+  redirectToWhatsApp(phoneNumber: string) {
+    const url = `https://wa.me/${phoneNumber}`;
+    window.open(url, '_blank');
+  }
 
-      emailData = {
-        to_name: '',
-        from_name: '',
-        message: '',
-        reply_to: ''
-      };
-    
-      constructor(private emailService: ServiceEmailService) {}
-    
-      sendEmail() {
-        this.emailService.sendMail(this.emailData)
-          .then(() => {
-            Swal.fire({
-              title: 'E-mail envoyé !',
-              text: 'Votre message a été envoyé avec succès.',
-              icon: 'success',
-              confirmButtonText: 'OK'
-            });
-      
-            // Réinitialiser le formulaire après l'envoi
-            this.emailData = {         
-                to_name: '',
-                from_name: '',
-                message: '',
-                reply_to: ''
-         };
-          })
-          .catch(error => {
-            Swal.fire({
-              title: 'Erreur',
-              text: 'Erreur lors de l’envoi : ' + error.text,
-              icon: 'error',
-              confirmButtonText: 'OK'
-            });
-          });
-      }
+
+  emailData = {
+    to_name: '',
+    from_name: '',
+    message: '',
+    reply_to: ''
+  };
+
+  constructor(private emailService: ServiceEmailService) { }
+
+  sendEmail() {
+    this.emailService.sendMail(this.emailData)
+      .then(() => {
+        Swal.fire({
+          title: 'E-mail envoyé !',
+          text: 'Votre message a été envoyé avec succès.',
+          icon: 'success',
+          confirmButtonText: 'OK'
+        });
+
+        // Réinitialiser le formulaire après l'envoi
+        this.emailData = {
+          to_name: '',
+          from_name: '',
+          message: '',
+          reply_to: ''
+        };
+      })
+      .catch(error => {
+        Swal.fire({
+          title: 'Erreur',
+          text: 'Erreur lors de l’envoi : ' + error.text,
+          icon: 'error',
+          confirmButtonText: 'OK'
+        });
+      });
+  }
 }
